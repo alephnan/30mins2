6f8e199e458bfb880020946e7e28f373842a0a30
Tuan Vo <automatwon@gmail.com>
2017-10-14 02:47:31 -0700

    Localize the graph data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,25 +12,6 @@
 import vis from 'vis';
 import store from '../store';
 
-const nodes = new vis.DataSet([
-  { id: 1, label: 'Node 1' },
-  { id: 2, label: 'Node 2' },
-  { id: 3, label: 'Node 3' },
-  { id: 4, label: 'Node 4' },
-]);
-
-const edges = new vis.DataSet([
-  { from: 1, to: 3 },
-  { from: 1, to: 2 },
-  { from: 2, to: 4 },
-]);
-
-const data = {
-  nodes,
-  edges,
-};
-const options = {};
-
 export default {
   name: 'HelloWorld',
   data() {
@@ -47,7 +28,11 @@ export default {
   methods: {
     loadGraph() {
       store.commit('increment');
-      console.log(store.state.count);
+      const data = {
+        nodes: new vis.DataSet(store.state.nodes),
+        edges: new vis.DataSet(store.state.edges),
+      };
+      const options = {};
       const container = document.getElementById('graph');
       const network = new vis.Network(container, data, options);
       console.log(network);
--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,17 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     count: 0,
+    nodes: [
+      { id: 1, label: 'Node 1' },
+      { id: 2, label: 'Node 2' },
+      { id: 3, label: 'Node 3' },
+      { id: 4, label: 'Node 4' },
+    ],
+    edges: [
+      { from: 1, to: 3 },
+      { from: 1, to: 2 },
+      { from: 2, to: 4 },
+    ],
   },
   mutations: {
     increment(state) {
