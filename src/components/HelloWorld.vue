15c7f88fb44cc27686f142e7ee9c33bbbedcd5b3
Tuan Vo <automatwon@gmail.com>
2017-10-14 02:07:33 -0700

    Load dummy graph

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,34 @@
   <div class="hello">
     <!--<h1>{{ msg }}</h1>-->
     <button v-on:click="loadGraph">Load</button>
+    <div id="graph">
+    </div>
   </div>
 </template>
 
 <script>
+import vis from 'vis';
+
+const nodes = new vis.DataSet([
+  { id: 1, label: 'Node 1' },
+  { id: 2, label: 'Node 2' },
+  { id: 3, label: 'Node 3' },
+  { id: 4, label: 'Node 4' },
+]);
+
+const edges = new vis.DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+]);
+
+const data = {
+  nodes,
+  edges,
+};
+const options = {};
+
+
 export default {
   name: 'HelloWorld',
   data() {
@@ -15,6 +39,10 @@ export default {
   },
   methods: {
     loadGraph() {
+      const container = document.getElementById('graph');
+      const network = new vis.Network(container, data, options);
+      console.log(JSON.stringify(vis));
+      console.log(JSON.stringify(network));
       console.log('hi');
     },
   },
